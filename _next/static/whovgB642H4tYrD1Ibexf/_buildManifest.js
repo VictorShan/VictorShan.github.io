4b979ab02c82830f1e011f79b01644eada35328c
Victor Shan <victorshanvs@gmail.com>
2020-08-28 15:29:05 -0700

    Deploy Next.js to gh-pages

--- _next/static/whovgB642H4tYrD1Ibexf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002F1bae71855718460c94aa.css","static\u002Fchunks\u002Fpages\u002Findex-ad0766786dd9b9e1f454.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
