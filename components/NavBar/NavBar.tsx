ec7e8af74e4ceb7a3ea9588739934d23a95a5aad
Victor Shan <victorshanvs@gmail.com>
2020-09-09 17:30:07 -0700

    nav links go to first page even on 404

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -3,7 +3,7 @@ import styles from './NavBar.module.sass'
 export default function NavBar() {
   return (
     <nav className={"navbar navbar-expand navbar-light sticky-top " + styles.navbar}>
-      <a className={"navbar-brand"} href={"#"}>
+      <a className={"navbar-brand"} href={"/#"}>
         <img 
           src={"/images/EagleLogo.jpg"}
           width="30"
@@ -15,8 +15,8 @@ export default function NavBar() {
       </a>
       <div className="navbar-nav-scroll">
         <div className="navbar-nav bd-navbar-nav flex-row">
-          <a className="nav-link active" href="#home">Home <span className="sr-only">(current)</span></a>
-          <a className="nav-link active" href="#projects">Projects</a>
+          <a className="nav-link active" href="/#home">Home <span className="sr-only">(current)</span></a>
+          <a className="nav-link active" href="/#projects">Projects</a>
           {/* {<a className="nav-link" href="#">Pricing</a>} */}
           {/* {<a className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</a>} */}
         </div>
