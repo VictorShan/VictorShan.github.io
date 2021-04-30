bef35cf3c61a736613d78a5761b450771a27a0ff
Victor Shan <victorshanvs@gmail.com>
2021-04-29 18:18:40 -0700

    Deploy Next.js to github.io

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "74fa00d12dce469724d8";
+/******/ 	var hotCurrentHash = "2125b95eaa8f38cb5359";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
