81da92a5d4c6111866b8981977b87b212b0756a4
Victor Shan <victorshanvs@gmail.com>
2021-07-06 00:46:16 -0700

    Deploy Next.js to github.io

---
+++ _next/static/lLSxlHqmZDa_vQJwmfZb3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7c1b1f07a54065f51d63.css","static/chunks/pages/index-3a729489ef992e9decba.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
