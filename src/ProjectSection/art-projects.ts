9fcb4c827d5433dd50facbf926a20b8d442e8acf
Victor Shan <victorshanvs@gmail.com>
2022-03-28 10:52:01 -0700

    add comma to blender donut desc

--- src/ProjectSection/art-projects.ts
+++ src/ProjectSection/art-projects.ts
@@ -10,7 +10,7 @@ const artProjects: project[] = [
   {
     projImg: "/images/donutThumbnail.png",
     projName: "Blender Donut",
-    projDesc: "The blender donut created using Blender Guru's donut tutorial. For best viewing experience right click the video and loop it.",
+    projDesc: "The blender donut created using Blender Guru's donut tutorial. For best viewing experience, right click the video and loop it.",
     website: "https://drive.google.com/file/d/1G7SGuInd-pLRe4JBN4h_0TVMNZxVYP87/preview"
   }
 ]
